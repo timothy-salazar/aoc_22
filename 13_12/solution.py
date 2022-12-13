309f3d5e56c8fbe4b0b7cad99f5452971df05785
timothy-salazar <timothy.salazar@colorado.edu>
2022-12-13 14:15:24 -0700

    finished part 2 for day 13

--- 13_12/solution.py
+++ 13_12/solution.py
@@ -1,4 +1,5 @@
 import json
+
 def get_data():
     with open('input', 'r') as f:
         data = f.read().strip()
@@ -28,6 +29,19 @@ def compare_packets(packet_1, packet_2):
     else:
         return len(packet_1) < len(packet_2)
 
+def flatten_data(parsed_data):
+    flattened = [packet for pair in parsed_data for packet in pair]
+    return flattened
+
+def crude_sort(packets):
+    for i in range(len(packets)):
+        j = i
+        while j > 0 and compare_packets(packets[j], packets[j-1]):
+            packets[j], packets[j-1] = packets[j-1], packets[j]
+            j -= 1
+    return packets
+
+
 def get_valid_indices(parsed_data):
     return [v+1 for v, pair in enumerate(parsed_data) if compare_packets(*pair)]
 
@@ -37,11 +51,20 @@ def get_solution_1(data):
     return sum(valid_indices)
 
 def get_solution_2(data):
-    pass
+    key_1 = [[2]]
+    key_2 = [[6]]
+    parsed_data = parse_data(data)
+    packets = flatten_data(parsed_data)
+    packets += [key_1, key_2]
+    packets = crude_sort(packets)
+    index_1 = packets.index(key_1) + 1
+    index_2 = packets.index(key_2) + 1
+    return index_1 * index_2
+
 
 if __name__ == "__main__":
     input_data = get_data()
     solution_1 = get_solution_1(input_data)
     print("Solution 1:", solution_1)
-    # solution_2 = get_solution_2(input_data)
-    # print("Solution 2:", solution_2)
+    solution_2 = get_solution_2(input_data)
+    print("Solution 2:", solution_2)
--- 13_12/test_solution.py
+++ 13_12/test_solution.py
@@ -1,6 +1,9 @@
-from solution import parse_data, compare_packets, get_valid_indices, get_solution_1
-from test_input import input_1, parsed_1, expected_1
+from solution import parse_data, compare_packets, get_valid_indices,\
+     get_solution_1, get_solution_2, flatten_data, crude_sort
+from test_input import input_1, parsed_1, expected_1, input_2
+import json
 
+# Part 1 tests
 def test_parser():
     parsed_data = parse_data(input_1)
     assert parsed_data == parsed_1
@@ -16,3 +19,23 @@ def test_valid_indices():
 
 def test_solution_1():
     assert get_solution_1(input_1) == 13
+
+# Part 2 tests
+def test_flattener():
+    parsed_data = parse_data(input_1)
+    assert len(parsed_data) == 8
+    flat = flatten_data(parsed_data)
+    assert len(flat) == 16
+
+def test_sort():
+    parsed_data = parse_data(input_1)
+    flat = flatten_data(parsed_data)
+    flat += [[[2]], [[6]]]
+    expected = [json.loads(line) for line in input_2.split('\n')]
+    flat = crude_sort(flat)
+    assert flat == expected
+    assert flat.index([[2]]) + 1 == 10
+    assert flat.index([[6]]) + 1 == 14
+
+def test_solution_2():
+    assert get_solution_2(input_1) == 140
